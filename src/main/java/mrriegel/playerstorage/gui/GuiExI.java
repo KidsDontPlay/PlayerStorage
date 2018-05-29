270dd35117f3f55e0cf9a7b77d2b438b9882b9a1
gaudus <ichhabads@gmail.com>
2018-05-29 19:22:37 +0200

    show potions

--- src/main/java/mrriegel/playerstorage/gui/GuiExI.java
+++ src/main/java/mrriegel/playerstorage/gui/GuiExI.java
@@ -4,12 +4,15 @@ import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.Validate;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+
+import com.google.common.collect.Ordering;
 
 import baubles.client.gui.GuiPlayerExpanded;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
@@ -39,6 +42,7 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
@@ -47,6 +51,8 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumFacing.Plane;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextFormatting;
@@ -63,7 +69,7 @@ public class GuiExI extends CommonGuiContainer {
 	public List<FluidStack> fluids = new ArrayList<>();
 	protected List<AbstractSlot<?>> slots = new ArrayList<>();
 	protected long lastClick;
-	protected CommonGuiButton sort, direction, clear, jei, modeButton, inc, dec, defaultt;
+	protected CommonGuiButton sort, direction, clear, jei, modeButton, inc, dec, defaultt, potion;
 	protected GuiTextField searchBar;
 	protected int currentPos = 0, maxPos = 0;
 	protected AbstractSlot<?> over;
@@ -94,6 +100,7 @@ public class GuiExI extends CommonGuiContainer {
 			xSize += 22;
 		ySize = 112 + 18 * gridHeight;
 		super.initGui();
+
 		searchBar = new GuiTextField(0, fontRenderer, guiLeft + 9, guiTop + 14 + 18 * gridHeight, 85, fontRenderer.FONT_HEIGHT);
 		searchBar.setMaxStringLength(30);
 		searchBar.setTextColor(16777215);
@@ -142,6 +149,11 @@ public class GuiExI extends CommonGuiContainer {
 						}
 					});
 			}
+		}
+		updateActivePotionEffects();
+		potion = null;
+		if (hasActivePotionEffects && guiLeft < 160) {
+			buttonList.add(potion = new CommonGuiButton(12000, guiLeft + 62, guiTop + 88 + 18 * gridHeight, 16, 16, "").setFrameColor(0xFF444444).setButtonColor(0xFF999999).setDesign(Design.SIMPLE).setStack(new ItemStack(Items.POTIONITEM)));
 		}
 		updateScreen();
 	}
@@ -227,6 +239,12 @@ public class GuiExI extends CommonGuiContainer {
 			lis.add(TextFormatting.AQUA + "- " + TextFormatting.RESET + "Hover over an item in your inventory and press " + ClientProxy.OPENLIMIT.getDisplayName() + " to adjust the limit.");
 			lis.add(TextFormatting.AQUA + "- " + TextFormatting.RESET + "Hover over an item/fluid in your storage and press " + ClientProxy.HIGHLIGHT.getDisplayName() + " to highlight it.");
 			GuiDrawer.renderToolTip(lis, mouseX, mouseY);
+		}
+		if (hasActivePotionEffects) {
+			if (potion == null)
+				drawActivePotionEffects(true);
+			else if (potion.isMouseOver())
+				drawActivePotionEffects(false);
 		}
 
 	}
@@ -561,5 +579,81 @@ public class GuiExI extends CommonGuiContainer {
 			}
 		}
 		return false;
+	}
+
+	protected boolean hasActivePotionEffects;
+
+	protected void updateActivePotionEffects() {
+		boolean hasVisibleEffect = false;
+		for (PotionEffect potioneffect : this.mc.player.getActivePotionEffects()) {
+			Potion potion = potioneffect.getPotion();
+			if (potion.shouldRender(potioneffect)) {
+				hasVisibleEffect = true;
+				break;
+			}
+		}
+		if (this.mc.player.getActivePotionEffects().isEmpty() || !hasVisibleEffect) {
+			this.guiLeft = (this.width - this.xSize) / 2;
+			this.hasActivePotionEffects = false;
+		} else {
+			if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.GuiScreenEvent.PotionShiftEvent(this)))
+				this.guiLeft = (this.width - this.xSize) / 2;
+			else
+				this.guiLeft = 160 + (this.width - this.xSize - 200) / 2;
+			this.hasActivePotionEffects = true;
+		}
+	}
+
+	private void drawActivePotionEffects(boolean left) {
+		double z = 605;
+		GlStateManager.translate(0, 0, z);
+		int i = left ? this.guiLeft - 124 - 35 : guiLeft;
+		int j = this.guiTop;
+		Collection<PotionEffect> collection = this.mc.player.getActivePotionEffects();
+
+		if (!collection.isEmpty()) {
+			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			GlStateManager.disableLighting();
+			int l = 33;
+
+			if (collection.size() > 5) {
+				l = 132 / (collection.size() - 1);
+			}
+
+			for (PotionEffect potioneffect : Ordering.natural().sortedCopy(collection)) {
+				Potion potion = potioneffect.getPotion();
+				if (!potion.shouldRender(potioneffect))
+					continue;
+				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+				this.mc.getTextureManager().bindTexture(INVENTORY_BACKGROUND);
+				this.drawTexturedModalRect(i, j, 0, 166, 140, 32);
+
+				if (potion.hasStatusIcon()) {
+					int i1 = potion.getStatusIconIndex();
+					this.drawTexturedModalRect(i + 6, j + 7, 0 + i1 % 8 * 18, 198 + i1 / 8 * 18, 18, 18);
+				}
+
+				potion.renderInventoryEffect(i, j, potioneffect, mc);
+				if (!potion.shouldRenderInvText(potioneffect)) {
+					j += l;
+					continue;
+				}
+				String s1 = I18n.format(potion.getName());
+
+				if (potioneffect.getAmplifier() == 1) {
+					s1 = s1 + " " + I18n.format("enchantment.level.2");
+				} else if (potioneffect.getAmplifier() == 2) {
+					s1 = s1 + " " + I18n.format("enchantment.level.3");
+				} else if (potioneffect.getAmplifier() == 3) {
+					s1 = s1 + " " + I18n.format("enchantment.level.4");
+				}
+
+				this.fontRenderer.drawStringWithShadow(s1, (float) (i + 10 + 18), (float) (j + 6), 16777215);
+				String s = Potion.getPotionDurationString(potioneffect, 1.0F);
+				this.fontRenderer.drawStringWithShadow(s, (float) (i + 10 + 18), (float) (j + 6 + 10), 8355711);
+				j += l;
+			}
+		}
+		GlStateManager.translate(0, 0, -z);
 	}
 }
