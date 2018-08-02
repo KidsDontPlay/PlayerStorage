5785272b60d311ba692bb71478ebe5016706212d
gaudus <ichhabads@gmail.com>
2018-08-02 14:14:17 +0200

    fixed #34

--- src/main/java/mrriegel/playerstorage/gui/GuiInfo.java
+++ src/main/java/mrriegel/playerstorage/gui/GuiInfo.java
@@ -20,6 +20,7 @@ import mrriegel.limelib.helper.NBTHelper;
 import mrriegel.limelib.network.PacketHandler;
 import mrriegel.limelib.util.Utils;
 import mrriegel.playerstorage.ClientProxy;
+import mrriegel.playerstorage.ConfigHandler;
 import mrriegel.playerstorage.Enums.MessageAction;
 import mrriegel.playerstorage.ExInventory;
 import mrriegel.playerstorage.Message2Server;
@@ -67,17 +68,17 @@ public class GuiInfo extends CommonGuiScreenSub {
 				int w = 208, h = 11;
 				int c = 0xffffc800;
 				drawer.drawColoredRectangle(10, 13, w, 11, ColorHelper.darker(c, .5));
-				double foo = ei.getItemCount() / (double) ei.itemLimit;
+				double foo = ConfigHandler.infiniteSpace ? .5 : ei.getItemCount() / (double) ei.itemLimit;
 				drawer.drawColoredRectangle(10, 13, (int) (w * foo), h, c);
 				drawer.drawFrame(10, 13, w, h, 1, 0xFF000000);
-				fontRenderer.drawString((isShiftKeyDown() ? ei.getItemCount() : Utils.formatNumber(ei.getItemCount())) + "/" + (isShiftKeyDown() ? ei.itemLimit : Utils.formatNumber(ei.itemLimit)) + " Items", guiLeft + 11, guiTop + 28, 0x3e3e3e);
+				fontRenderer.drawString((isShiftKeyDown() ? ei.getItemCount() : Utils.formatNumber(ei.getItemCount())) + "/" + (ConfigHandler.infiniteSpace ? "" : isShiftKeyDown() ? ei.itemLimit : Utils.formatNumber(ei.itemLimit)) + " Items", guiLeft + 11, guiTop + 28, 0x3e3e3e);
 
 				c = 0xff485ec9;
 				drawer.drawColoredRectangle(10, 45, w, h, ColorHelper.darker(c, .5));
-				foo = ei.getFluidCount() / (double) ei.fluidLimit;
+				foo = ConfigHandler.infiniteSpace ? .5 : ei.getFluidCount() / (double) ei.fluidLimit;
 				drawer.drawColoredRectangle(10, 45, (int) (w * foo), h, c);
 				drawer.drawFrame(10, 45, w, h, 1, 0xFF000000);
-				fontRenderer.drawString((isShiftKeyDown() ? ei.getFluidCount() : Utils.formatNumber(ei.getFluidCount())) + "/" + (isShiftKeyDown() ? ei.fluidLimit : Utils.formatNumber(ei.fluidLimit)) + " mB", guiLeft + 11, guiTop + 60, 0x3e3e3e);
+				fontRenderer.drawString((isShiftKeyDown() ? ei.getFluidCount() : Utils.formatNumber(ei.getFluidCount())) + "/" + (ConfigHandler.infiniteSpace ? "" : isShiftKeyDown() ? ei.fluidLimit : Utils.formatNumber(ei.fluidLimit)) + " mB", guiLeft + 11, guiTop + 60, 0x3e3e3e);
 
 				String inter = "Interfaces";
 				drawer.drawColoredRectangle(10, 78, fontRenderer.getStringWidth(inter) + 2, fontRenderer.FONT_HEIGHT + 1, 0xffe1e1e1);
